936f7712a47b24b6213203e3b9f4d30a69cd6df6
Washington <washington.contato1327@gmail.com>
2025-07-11 11:40:46 -0300

    chore: criar container PostgreSQL com Docker, configurar database.js e conectar backend ao banco

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // ajustado aqui
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+import config from "./config/database.js";
 
 const app = express();
 
-const PORT = 3003;
+const sequelize = new Sequelize(config);
+
+const PORT = process.env.PORT || 3002;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
