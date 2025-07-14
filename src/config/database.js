5a3645a363d75cef2ace1b6e67c50b388c1e6fc1
Washington <washington.contato1327@gmail.com>
2025-07-14 13:54:39 -0300

    chore: ajustar configuração do banco para usar DATABASE_URL no Render e variáveis locais no desenvolvimento

--- src/config/database.js
+++ src/config/database.js
@@ -18,22 +18,30 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default {
+  ...(isProduction
+    ? {
+        url: process.env.DATABASE_URL, // usa a DATABASE_URL no Render
+      }
+    : {
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+      }),
   dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // importante para Supabase
+    },
+  },
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
-  },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // importante para evitar erro de certificado
-    },
   },
 };
