010a7122aae7a0ef2389e204b57f97b1fd15823e
Washington <washington.contato1327@gmail.com>
2025-07-14 13:47:11 -0300

    Ajustes na conexão com o banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,39 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
+// dotenv.config();
+
+// export default {
+//   dialect: "postgres",
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD, // ajustado aqui
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
   dialect: "postgres",
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // ajustado aqui
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // importante para evitar erro de certificado
+    },
   },
 };
