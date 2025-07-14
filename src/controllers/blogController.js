582b92f68809899aaba794bc18e18ea141be7c7e
Washington <washington.contato1327@gmail.com>
2025-07-13 21:14:28 -0300

    Adiciona funções no controller de editar e exluit post, Ajusta novas rotas

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -34,3 +34,45 @@ export const getPosts = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const deletPosts = async (req, res) => {
+  try {
+    const deletePost = await Blog.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (deletePost) {
+      res.status(200).json({ message: "Post deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(409).json({ message: "Error deleting Post" });
+  }
+};
+
+export const editPost = async (req, res) => {
+  try {
+    const [updatedPost] = await Blog.update(
+      {
+        title: req.body.title,
+        author: req.body.author,
+        cover_image: req.body.cover_image,
+        summary: req.body.summary,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    const post = await Blog.findByPk(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/blogController.js";
+import {
+  createPost,
+  deletPosts,
+  editPost,
+  getPosts,
+} from "../controllers/blogController.js";
 
 const router = express.Router();
 router.post("/createPost", createPost);
 router.get("/getPosts", getPosts);
+router.delete("/deletePost/:id", deletPosts);
+router.put("/editPost/:id", editPost);
 
 export default router;
