e9b2cc1ac08cec0ef90f3e714ff09515aeb0d2ca
Washington <washington.contato1327@gmail.com>
2025-07-15 11:02:21 -0300

    Configuração de ambiente e sequelize para produção e desenvolvimento

--- src/config/database.js
+++ src/config/database.js
@@ -7,22 +7,26 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default {
   dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  ...(isProduction
+    ? {
+        use_env_variable: "DATABASE_URL",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+      }),
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-  dialectOptions: isProduction
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      }
-    : {},
 };
