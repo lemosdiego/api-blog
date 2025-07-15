2419fdbedba6d8a7e207a409d75685da5f7e3865
Washington <washington.contato1327@gmail.com>
2025-07-15 09:07:38 -0300

    Atualizações finais para deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,44 +1,15 @@
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// export default {
-//   dialect: "postgres",
-//   host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD, // ajustado aqui
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//     underscoredAll: true,
-//   },
-// };
 import dotenv from "dotenv";
-dotenv.config();
 
-const isProduction = process.env.NODE_ENV === "production";
+const envFile = process.env.NODE_ENV === "development" ? ".env.local" : ".env";
+dotenv.config({ path: envFile });
 
 export default {
-  ...(isProduction
-    ? {
-        url: process.env.DATABASE_URL, // usa a DATABASE_URL no Render
-      }
-    : {
-        host: process.env.DB_HOST,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-      }),
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // importante para Supabase
-    },
-  },
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
   define: {
     timestamps: true,
     underscored: true,
